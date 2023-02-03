171691a35379a181588fd0e698d13ad5f8d312ce
greenbsk <qhqh147@gmail.com>
2023-02-03 14:03:52 +0900

    Initial Commit

---
+++ BSK_testgit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BSK_testgit
+//
+//  Created by BS K on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
